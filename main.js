0f53d59db22364ab4ead930f64d14cb07a094b3d
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2017-07-12 16:33:12 -0400

    creating uno cards

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ let colorArray= ["red", "green", "blue", "yellow"],
 colorArray.forEach(function(color) {
     for (let number in oneThruNine){
         cardValue=parseInt(number); 
-        let newCard= new UnoCard(color, cardValue, null); 
-        if (cardValue<9){
+        let newNumberCard= new UnoCard(color, cardValue, null); 
+        if (cardValue===0){
             let newActionCard= new UnoCard(color, null, "DrawTwoCards");
                 cardDeck.push(newActionCard);
                 cardDeck.push(newActionCard);
@@ -26,31 +26,36 @@ colorArray.forEach(function(color) {
                 newActionCard= new UnoCard(color, null, "SkipCards");
                 cardDeck.push(newActionCard);
                 cardDeck.push(newActionCard);
-                
-                //Skip cards
+                cardDeck.push(newNumberCard);
         }
-
-        if (cardValue===0){
-                cardDeck.push(newCard);
-        }
-
         else{
-                cardDeck.push(newCard);
-                cardDeck.push(newCard);
-        }
-
-        
-           
+                cardDeck.push(newNumberCard);
+                cardDeck.push(newNumberCard);
+        }           
     }
-   
-
+    let newWildCard= new UnoCard(null, null, "Wild"); 
+    let newDrawFourCard= new UnoCard(null, null, "DrawFour"); 
+        cardDeck.push(newWildCard);
+        cardDeck.push(newDrawFourCard);
 });
 
 
 /*
 8 Draw Two cards – 2 each in blue, green, red, and yellow
 8 Reverse cards – 2 each in blue, green, red, and yellow
-8 Skip cards – 2 each in blue, green, red, and yellow       */
+8 Skip cards – 2 each in blue, green, red, and yellow  
+
+4x Wild
+This card represents all four colors, and can be placed on any card. The player 
+has to state which color it will represent for the next player. It can be played
+ regardless of whether another card is available.
+ 
+4x Wild Draw Four
+This acts just like the wild card except that the next player also has to draw 
+four cards. With this card, you must have no other alternative cards to play that 
+matches the color of the card previously played. If you play this card illegally,
+ you may be challenged by the other player to show your hand. If guilty, you need 
+ to draw 4 cards. If not, the challenger needs to draw 6 cards instead.     */
 
 
 console.log (cardDeck); 
